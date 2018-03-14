f9c6137ae5fdb525204f5e35151ef590e9066e19
Rachel Miles <milesrachel9@gmail.com>
2018-03-14 11:25:11 -0600

    unbreak, add routes and apps

---
+++ client/src/components/Apps.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getApps } from '../actions/apps';
+import { Container, Header, Card, Image } from 'semantic-ui-react';
+
+class Apps extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getApps())
+  }
+
+apps = () => {
+  return this.props.apps.map( app=>
+    <Card key={app.id}>
+      <Image src={app.logo} />
+      <Card.Content>
+        <Card.Header>
+          {app.name}
+        </Card.Header>
+        <Card.Meta>
+          {app.author}
+        </Card.Meta>
+        <Card.Description>
+          {app.category}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Link to={'/apps/${app.id}'}>
+          View App
+        </Link>
+      </Card.Content>
+    </Card>
+  )
+}
+
+  render() {
+    return (
+      <Container>
+        <Header as='h3' textAlign='center'>Apps</Header>
+        <Card.Group itemsPerRow={4}>
+          { this.apps() }
+        </Card.Group>
+      </Container>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { apps: state.apps }
+}
+
+export default connect(mapStateToProps)(Apps);
