52b8a8d3f7dac7f0a183e3ccfbfff246412c81ac
Rachel Miles <milesrachel9@gmail.com>
2018-03-14 09:55:30 -0600

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
